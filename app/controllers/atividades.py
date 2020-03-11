4042d2e9fad84c32a4f5112ae75d0140623215cf
Manoel <manoel_gomes@outlook.com>
2020-03-10 20:37:18 -0400

    Aula 4

--- app/controllers/atividades.py
+++ app/controllers/atividades.py
@@ -1,5 +1,9 @@
+import os
+import uuid
+
 from app import app
 from flask import render_template
+from flask import request
 from app.models.tables import Atividade
 
 
@@ -8,6 +12,21 @@ from app.models.tables import Atividade
 @app.route('/atividades')
 def listar_atividades():
     a = Atividade.query.all()
-    for i in range (len(a)):
+    for i in range(len(a)):
         a[i].data = a[i].data.strftime('%d/%m/%Y')
     return render_template("atividades_listar.html", atividades=a)
+
+#Carregando o formulario
+@app.route('/atividades/novo')
+def form_nova_atividade():
+    tipo = 'inserir'
+    return render_template("atividades_formulario.html", tipo=tipo)
+
+#Cadastrar uma nova atividade
+@app.route('/atividades/novo', methods=['POST'])
+def inserir_atividade():
+    #Recebendo arquivo
+    arquivo = request.files['arquivo']
+    arquivo.filename = str(uuid.uuid4()) + os.path.splitext(arquivo.filename)[1]
+    print(arquivo.filename)
+    return ""
